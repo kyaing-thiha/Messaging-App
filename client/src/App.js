41f8b21728d1523944393a7f91a3963ef939bd36
Kyaing Thiha <kyaingthiha94@gmail.com>
2020-03-22 18:59:01 +1100

    Defined basic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       {/* For Page Layout */}
       <div className = "chat-screen-container">
-        
+        <div className = "toolbar" >
+          <span className="application-name">Kyaing's Minions</span>
+          <span className="profile-pic"></span>
+        </div>
+        <div className="chat-main-body-container">
+            <div className="side-bar">
+              
+            </div>
+            <div className="message-screen">
+              <div className="message-screen-title">
+              Minion1
+              </div>
+              <div className="message-display-area">
+                
+              </div>
+              <div className="text-area-container">
+                <input type="text" className="text-area">
+                </input>
+              </div>
+              
+            </div>
+        </div>
       </div>
     </div>
   );

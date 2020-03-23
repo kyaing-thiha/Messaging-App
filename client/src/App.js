1777a9cde777ecdbb36706b627ad9ba5746a0c08
Kyaing Thiha <kyaingthiha94@gmail.com>
2020-03-23 12:57:17 +1100

    Separate Message Page to different file

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./App.css";
 
-import ToolBar from "./components/ChatToolBar"
-import SideBar from "./components/ChatSideBar"
-import MessageScreen from "./components/MessageScreen"
+import ChatPage from "./page/ChatPage"
 
-/**
-  Mock Data
-*/
-import {user1, user2} from "./mocks/mockUsers"
-
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      user: user1,
-      selectedReceiver: null,
-    }
-  }
-
-  setSelectedReceiver = (receiver) => {
-    this.setState({selectedReceiver: receiver})
-  }
-
-  render(){
-    const { user } = this.state;
-    return (
-      <div className="chat-screen-container">
-        <ToolBar profilePic={this.state.user.profilePic}/>
-        <div className="chat-main-body-container">
-          <SideBar 
-            allReceivers = {user.minionPals} 
-            onSelectReceiver = {this.setSelectedReceiver}
-            selectedReceiver= {this.state.selectedReceiver}
-          />
-          <MessageScreen {...this.state} />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <ChatPage/>
+)
 
 export default App;

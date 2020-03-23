0814b1e1b2f4da42b150045997f5e93b2b961379
Kyaing Thiha <kyaingthiha94@gmail.com>
2020-03-23 17:46:04 +1100

    Separate Cors header into middleware

--- server/index.js
+++ server/index.js
@@ -1,31 +1,19 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
+
 const app = express();
 const PORT = 8080;
 
-//Mock Data
-const mockUser = require("./src/mocks/mockUser")
-const mockMessages = require("./src/mocks/mockMessages")
+const mockMiddleware = require("./src/middlewares/mockMiddlewares")
+const attachCorsHeader = require("./src/middlewares/attachCorsHeader")
 
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS"){
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-    next();
-})
+app.use(attachCorsHeader);
 
-app.get("/getMockUser", (req, res, next)=>{
-    res.json(mockUser)
-})
+//Mock
+app.use("/mock", mockMiddleware);
 
-app.get("/getMockMessages", (req, res, next)=>{
-    res.json(mockMessages)
-})
+//GraphQL
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`) 

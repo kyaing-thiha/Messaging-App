14f18697c29fbb2b678a021d2486f8f448fd5e51
Kyaing Thiha <kyaingthiha94@gmail.com>
2020-03-27 19:19:20 +1100

    Added signin, pending to t/s memory leak

--- client/src/page/LoginPage.js
+++ client/src/page/LoginPage.js
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPage.css"
+import { postURL } from "../utils"
+import { Redirect } from "react-router-dom"
 
-const Login = () => (
-    <div className="login-page">
-        <div className="login-container">
-            <div className="logo">
-                Kyaing's Minions
+const Login = (props) => {
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginSuccess, setLoginSuccess] = useState(false);
+    const [loginAttemptFailed, setLoginAttemptFailed] = useState(false);
+    let token = "";
+
+    const handleSignIn = async () => {
+        try {
+            const options = {
+                name: userName,
+                password
+            }
+            token = await postURL("http://localhost:8080/minions/signIn", options);
+            setLoginSuccess(true);
+        }
+        catch (error) {
+            setLoginAttemptFailed(true);
+        }
+    }
+
+    return (
+        loginSuccess ?
+            (<Redirect to="/Chat" />) :
+            (<div className="login-page">
+                <div className="login-container">
+                    <div className="logo">
+                        Kyaing's Minions
+                            </div>
+                    <div className="input-container">
+                        <input
+                            className="user-input"
+                            type="text"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                        />
+                    </div>
+                    <div className="input-container">
+                        <input
+                            className="user-input"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+
+                    <button
+                        className="login-button-container"
+                        onClick={handleSignIn} >
+                        Login
+                            </button>
+                </div>
             </div>
-            <div className="input-container">
-                <input 
-                    className="user-input" 
-                    type="text"/>
-            </div>
-            <div className="input-container">
-                <input 
-                    className="user-input" 
-                    type="password" />
-            </div>
-            <button className="login-button-container">
-                Login
-            </button>
-        </div>
-    </div>
-)
+        )
+    )
+}
+
+
 
 export default Login;
